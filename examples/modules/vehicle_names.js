f08016f9bdac9db8ce3d261903bc8ae541aec7d1
damo <damo@damo.com.au>
2015-05-30 01:52:36 +0930

    First commit

---
+++ examples/modules/vehicle_names.js
@@ -0,0 +1,59 @@
+var vehicleNames = [
+"Landstalker", "Bravura", "Buffalo", "Linerunner", "Perrenial", "Sentinel",
+	"Dumper", "Firetruck", "Trashmaster", "Stretch", "Manana", "Infernus",
+	"Voodoo", "Pony", "Mule", "Cheetah", "Ambulance", "Leviathan", "Moonbeam",
+    "Esperanto", "Taxi", "Washington", "Bobcat", "Whoopee", "BF Injection",
+	"Hunter", "Premier", "Enforcer", "Securicar", "Banshee", "Predator", "Bus",
+	"Rhino", "Barracks", "Hotknife", "Trailer", "Previon", "Coach", "Cabbie",
+	"Stallion", "Rumpo", "RC Bandit", "Romero", "Packer", "Monster", "Admiral",
+	"Squalo", "Seasparrow", "Pizzaboy", "Tram", "Trailer", "Turismo", "Speeder",
+	"Reefer", "Tropic", "Flatbed", "Yankee", "Caddy", "Solair", "Berkley's RC Van",
+	"Skimmer", "PCJ-600", "Faggio", "Freeway", "RC Baron", "RC Raider", "Glendale",
+	"Oceanic", "Sanchez", "Sparrow", "Patriot", "Quad", "Coastguard", "Dinghy",
+	"Hermes", "Sabre", "Rustler", "ZR-350", "Walton", "Regina", "Comet", "BMX",
+	"Burrito", "Camper", "Marquis", "Baggage", "Dozer", "Maverick", "News Chopper",
+	"Rancher", "FBI Rancher", "Virgo", "Greenwood", "Jetmax", "Hotring", "Sandking",
+	"Blista Compact", "Police Maverick", "Boxville", "Benson", "Mesa", "RC Goblin",
+	"Hotring Racer A", "Hotring Racer B", "Bloodring Banger", "Rancher", "Super GT",
+	"Elegant", "Journey", "Bike", "Mountain Bike", "Beagle", "Cropduster", "Stunt",
+ 	"Tanker", "Roadtrain", "Nebula", "Majestic", "Buccaneer", "Shamal", "Hydra",
+ 	"FCR-900", "NRG-500", "HPV1000", "Cement Truck", "Tow Truck", "Fortune",
+ 	"Cadrona", "FBI Truck", "Willard", "Forklift", "Tractor", "Combine", "Feltzer",
+ 	"Remington", "Slamvan", "Blade", "Freight", "Streak", "Vortex", "Vincent",
+    "Bullet", "Clover", "Sadler", "Firetruck", "Hustler", "Intruder", "Primo",
+	"Cargobob", "Tampa", "Sunrise", "Merit", "Utility", "Nevada", "Yosemite",
+	"Windsor", "Monster", "Monster", "Uranus", "Jester", "Sultan", "Stratium",
+	"Elegy", "Raindance", "RC Tiger", "Flash", "Tahoma", "Savanna", "Bandito",
+    "Freight Flat", "Streak Carriage", "Kart", "Mower", "Dune", "Sweeper",
+	"Broadway", "Tornado", "AT-400", "DFT-30", "Huntley", "Stafford", "BF-400",
+	"News Van", "Tug", "Trailer", "Emperor", "Wayfarer", "Euros", "Hotdog", "Club",
+	"Freight Box", "Trailer", "Andromada", "Dodo", "RC Cam", "Launch", "Police Car",
+ 	"Police Car", "Police Car", "Police Ranger", "Picador", "S.W.A.T", "Alpha",
+ 	"Phoenix", "Glendale", "Sadler", "Luggage", "Luggage", "Stairs", "Boxville",
+ 	"Tiller", "Utility Trailer"
+];
+
+exports = {
+	GetVehicleByName: function(name){
+		var idx = vehicleNames.indexOf(name);
+		if(idx > -1){
+			return [{ id: (idx*1)+400, name: name}];
+		}
+		else {
+			var matches = [];
+			var rx = new RegExp(name,"i");
+			for(var i in vehicleNames){
+				if(rx.test(vehicleNames[i])){
+					matches.push({ id: (i*1)+400, name: vehicleNames[i]});	
+				}
+			}
+			
+			print(matches);
+			return matches;
+		}
+	},
+	
+	GetVehicleName: function(typeid){
+		return vehicleNames[(typeid*1)-400];		
+	}
+};
