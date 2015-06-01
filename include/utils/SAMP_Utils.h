38806aa0c570c11710e1c907309f75066b496245
damo <damo@damo.com.au>
2015-06-01 16:35:17 +0930

    -

--- include/utils/SAMP_Utils.h
+++ include/utils/SAMP_Utils.h
@@ -8,7 +8,7 @@ public:
 	SAMP_Utils(SAMP_JS* sampjs);
 
 	static void Print(const FunctionCallbackInfo<Value> & args);
-	static void SAMP_Utils::PrintObject(Isolate* isolate, Local<Value> name_, Local<Value> value, int level);
+	static void PrintObject(Isolate* isolate, Local<Value> name_, Local<Value> value, int level);
 };
 
 #endif
